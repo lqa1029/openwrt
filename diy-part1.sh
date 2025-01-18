560e91b982bd3dc1d6d4662eb9a466205b41007d
lqa1029 <33282404+lqa1029@users.noreply.github.com>
2025-01-18 13:51:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git opentopd https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
-echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
